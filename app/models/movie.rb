acc65f22ccb3328933b1ebddd90c03886032e31a
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-24 08:32:05 -0400

    Associates the tables of movies and actors together

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+
+  has_many :actors 
+
   validates :title, presence: true
   validates :year, presence: true
   validates :plot, presence: true, length: { minimum: 10 }
