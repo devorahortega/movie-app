b0172a7b37735e6edc5a0b373a6816bcc09b3dca
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-18 19:34:22 -0500

    Attempts to fix routes to make frontend work

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,8 +3,8 @@ class Movie < ApplicationRecord
   belongs_to :movie_genres
   has_many :genres, through: :movie_genres
 
-  validates :title, presence: true
-  validates :year, presence: true
-  validates :plot, presence: true, length: { minimum: 10 }
-  validates :director, presence: true
+  # validates :title, presence: true
+  # validates :year, presence: true
+  # validates :plot, presence: true, length: { minimum: 10 }
+  # validates :director, presence: true
 end
